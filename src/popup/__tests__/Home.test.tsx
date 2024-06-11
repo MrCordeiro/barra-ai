52801c7040ff9fb8625fc23cbc5945ed79bfa227
Fernando Cordeiro <fernando_cordeiro@live.com>
2024-06-11 10:52:46 +0100

    📦 NEW: Test utils

--- src/popup/__tests__/Home.test.tsx
+++ src/popup/__tests__/Home.test.tsx
@@ -1,5 +1,5 @@
-import { render, screen } from '@testing-library/react';
-import Home from './Home';
+import { render, screen } from '../../../jest/test-utils';
+import Home from '../components/Home';
 
 describe('<Home />', () => {
   test('should render correctly', () => {
