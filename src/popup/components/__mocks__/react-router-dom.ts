0fda64d6b9416f5a211c54c204fe078b64be2607
Fernando Cordeiro <fernando_cordeiro@live.com>
2024-06-11 10:25:32 +0100

    📦 NEW: Chakra UI

--- src/popup/components/__mocks__/react-router-dom.ts
+++ src/popup/components/__mocks__/react-router-dom.ts
@@ -3,6 +3,14 @@ import { jest } from '@jest/globals';
 const originalModule =
   jest.requireActual<typeof import('react-router-dom')>('react-router-dom');
 
+/**
+ * The useNavigate and useLocation hooks will be replaced by mocks in the tests.
+ * You can control their behavior by importing them from react-router-dom and
+ * mocking their return values. For example:
+ *
+ * >>> import { useLocation } from 'react-router-dom';
+ * >>> (useLocation as jest.Mock).mockReturnValue({ pathname: '/' });
+ */
 module.exports = {
   ...originalModule,
   useNavigate: jest.fn(),
--- src/popup/main.tsx
+++ src/popup/main.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { ChakraProvider, ColorModeScript, ThemeConfig } from '@chakra-ui/react';
+import App from './App.tsx';
+import theme from './theme';
+
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ColorModeScript
+      initialColorMode={(theme.config as ThemeConfig).initialColorMode}
+    />
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>
+);
