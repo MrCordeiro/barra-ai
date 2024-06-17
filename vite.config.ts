86db602726dac2481bf0b5d872d89eafebf85df7
Fernando Cordeiro <fernando_cordeiro@live.com>
2024-06-17 11:44:06 +0100

    📦 NEW: Zips build

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import { crx } from '@crxjs/vite-plugin';
+import zipPack from 'vite-plugin-zip-pack';
 import manifest from './manifest.json';
 
 // https://vitejs.dev/config/
@@ -11,7 +12,7 @@ export default defineConfig(({ mode }) => {
       'process.env.USE_MOCK': env.USE_MOCK,
       'process.env.OPENAI_API_KEY': JSON.stringify(env.OPENAI_API_KEY),
     },
-    plugins: [react(), crx({ manifest })],
+    plugins: [react(), crx({ manifest }), zipPack()],
     server: {
       port: 3000,
     },
