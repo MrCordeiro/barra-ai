5ef9d9a2956ff08e88cde61065a2a1cb52f8da71
Fernando Cordeiro <fernando_cordeiro@live.com>
2026-03-29 16:20:43 +0100

    🚀 RELEASE: 1.4.0
    Signed-off-by: Fernando Cordeiro <fernando_cordeiro@live.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,6 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import { crx } from '@crxjs/vite-plugin';
 import zipPack from 'vite-plugin-zip-pack';
-import manifest from './manifest.json';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -10,9 +8,8 @@ export default defineConfig(({ mode }) => {
   return {
     define: {
       'process.env.USE_MOCK': env.USE_MOCK,
-      'process.env.OPENAI_API_KEY': JSON.stringify(env.OPENAI_API_KEY),
     },
-    plugins: [react(), crx({ manifest }), zipPack()],
+    plugins: [react(), zipPack()],
     server: {
       port: 3000,
     },
